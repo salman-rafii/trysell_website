bf8c64f81ff1afabf38535fde99a78cd2c32c403
salmanrafi4 <salmanrafi4@gmail.com>
2022-04-05 08:52:22 +0500

    Initial Commit

---
+++ android/app/src/main/kotlin/com/example/trysell_website/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.trysell_website
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/home/home_screen.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:trysell_website/constants.dart';
+import 'package:trysell_website/models/Blog.dart';
+import 'package:trysell_website/responsive.dart';
+
+import 'components/blog_post.dart';
+import 'components/categories.dart';
+import 'components/recent_posts.dart';
+import 'components/search.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Expanded(
+          flex: 2,
+          child: Column(
+            children: List.generate(
+              blogPosts.length,
+              (index) => BlogPostCard(blog: blogPosts[index]),
+            ),
+          ),
+        ),
+        if (!Responsive.isMobile(context)) SizedBox(width: kDefaultPadding),
+        // Sidebar
+        if (!Responsive.isMobile(context))
+          Expanded(
+            child: Column(
+              children: [
+                Search(),
+                SizedBox(height: kDefaultPadding),
+                Categories(),
+                SizedBox(height: kDefaultPadding),
+                RecentPosts(),
+              ],
+            ),
+          ),
+      ],
+    );
+  }
+}
